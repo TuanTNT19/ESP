bc5bffdbf537cca80dfaabc28fe79baa7c61f675
TuanTNT19 <tuantnt08@gmail.com>
2023-07-03 19:46:35 +0700

    update Version2

--- transmit.ino
+++ transmit.ino
@@ -5,7 +5,7 @@ String data;
 const char* ssid = "Oppo A12";
 const char* password = "oppo A12";
 
-#define TOKEN "Messi87" //Access token of device Display
+#define TOKEN "Messi879" //Access token of device Display
 char ThingsboardHost[] = "thingsboard.cloud";
 
 WiFiClient wifiClient;
@@ -14,7 +14,7 @@ int status = WL_IDLE_STATUS;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -34,31 +34,26 @@ if ( !client.connected() )
     reconnect();
 }
 getAndSendTemperatureAndHumidityData();
- // delay(1000);
+  delay(1000);
 }
 
 void getAndSendTemperatureAndHumidityData()
 {
   
-   if (Serial.available()) {
-    // delay(1000);          // Kiểm tra nếu có dữ liệu đang chờ đọc
-    data = Serial.readString();  // Đọc dữ liệu vào biến String
-    //Serial.println(data);
-   }
-  // Prepare a JSON payload string
-//    String payload = "{";
-//  payload += "\"Humidityuyt\":";payload += data.substring(4,8); 
-//   payload += "}";
-    String payload = "{";
- payload += "\"FireTest\":";payload += data.substring(0,4); payload += ",";
- payload += "\"TempTest\":";payload +=data.substring(4,8); 
-  payload += "}";
+    if (Serial.available()) {
+   delay(1000); 
+   data = Serial.readString();         
+       String payload = "{";
+    payload += "\"FireTest\":";payload += data.substring(0,4); payload += ",";
+   payload += "\"TempTest\":";payload +=data.substring(4,8); 
+    payload += "}";
+ 
 
   char attributes[1000];
   payload.toCharArray( attributes, 1000 );
   client.publish( "v1/devices/me/telemetry",attributes);
   Serial.println( attributes );
-   
+  }
 }
 
 void reconnect() {
@@ -75,7 +70,7 @@ void reconnect() {
     }
     Serial.print("Connecting to ThingsBoard node ...");
     // Attempt to connect (clientId, username, password)
-    if ( client.connect("Esp8266", TOKEN, NULL) ) {
+    if ( client.connect("ESPTESTk", TOKEN, NULL) ) {
       Serial.println( "[DONE]" );
     } else {
       Serial.print( "[FAILED] [ rc = " );
